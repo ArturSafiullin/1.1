5c0c4bf9da0dc0d83deb58007ab811572a088a4e
Artur Safiullin <artsafiullin@gmail.com>
2017-02-23 00:36:12 -0500

    Update Calculator.java

---
+++ chapter_002/Calculator/Calculator.java
@@ -0,0 +1,39 @@
+/** 
+ * Calculator
+ * @author asafiullin
+ * @since 21.02.2016
+ * @version 1
+*/
+package calculator;
+
+//Create a class
+public class Calculator {
+
+//Внутреннее поле 
+	private double result;
+	private double first
+	private double second;
+}
+
+	public static void main(double result) {
+			this.result = this.result + first;
+			this.result = this.result + second;		
+	}
+
+//Method add
+	public void add{
+
+		Calculator calculator = new Calculator();
+		double first = 1;
+		double second = 2;
+		getResult(calculator);
+		System.out.println(this.result);
+
+		}
+	public void getResult(Calculator getResult) {
+		CagetResult.result;
+	}
+
+
+
+}
