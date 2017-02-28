fea4ba24d5915f01416362b1137265e18254000d
ArturSafiullin <artsafiullin@gmail.com>
2017-02-27 22:09:01 -0500

    add

---
+++ chapter_001/src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,40 @@
+/** 
+ * Calculator
+ * @author asafiullin
+ * @since 21.02.2017
+ * @version 1
+*/
+package ru.job4j;
+
+//Create a class
+public class Calculator {
+
+//Внутреннее поле 
+	private double result;
+
+//Method add
+	public void add (double first, double second) {
+		this.result = first + second;
+		}
+
+//Method substract
+	public void substract (double first, double second) {
+		this.result = first - second;
+		}
+
+//Method div
+	public void div (double first, double second) {
+		this.result = first / second;
+		}
+
+//Method multiple
+	public void multiple (double first, double second) {
+		this.result = first * second;
+		}
+		
+//Method getResult
+	public double getResult () {
+		return result;
+	}
+
+}
